f7d22e456ed7ee4e9e84ebf488b8a8be20d38bf0
mall <mall@local>
2026-03-22 11:15:22 +0800

    修改

--- src/main/java/com/example/mall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/mall/service/impl/OrderServiceImpl.java
@@ -26,7 +26,7 @@ public class OrderServiceImpl implements OrderService {
         "Creating order (in-memory). productId={}, quantity={}, amount={}",
         request.getProductId(),
         request.getQuantity(),
-        request.getAmount()+"结束");
+        request.getAmount()+"结束日志打印");
 
     Order order = new Order();
     order.setId(idGenerator.getAndIncrement());
